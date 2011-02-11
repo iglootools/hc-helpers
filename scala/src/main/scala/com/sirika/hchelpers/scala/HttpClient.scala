1048c547060f771c8451c2dec3117d16b0278c65
Sami Dalouche <sami.dalouche@gmail.com>
2011-02-11 16:25:14 -0500

    [scala] made HttpClient object final

--- scala/src/main/scala/com/sirika/hchelpers/scala/HttpClient.scala
+++ scala/src/main/scala/com/sirika/hchelpers/scala/HttpClient.scala
@@ -5,7 +5,7 @@ import org.apache.http.client.{CookieStore, HttpClient}
 import com.sirika.hchelpers.core.DefaultHttpClientFactory
 import scala.collection.JavaConversions._
 
-object SimpleHttpClient {
+final object SimpleHttpClient {
   def apply(credentials: Map[AuthScope, Credentials]=Map(), params: Map[String, AnyRef]=Map(), cookieStore: Option[CookieStore]=None, shouldUseGzipCompression: Boolean=true): HttpClient = {
     DefaultHttpClientFactory.httpClient(credentials, params, cookieStore.getOrElse(null), shouldUseGzipCompression)
   }
