7bb394882b7257e138bb2c657d74c7c007a5471b
Sami Dalouche <sami.dalouche@gmail.com>
2011-02-16 11:59:39 -0500

    changed license year to 2011

--- java/src/main/java/com/sirika/hchelpers/java/HttpResponseCallback.java
+++ java/src/main/java/com/sirika/hchelpers/java/HttpResponseCallback.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2009 Sami Dalouche
+ * Copyright 2011 Sami Dalouche
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- scala/src/main/scala/com/sirika/hchelpers/scala/package.scala
+++ scala/src/main/scala/com/sirika/hchelpers/scala/package.scala
@@ -1,5 +1,5 @@
 /**
- * Copyright 2009 Sami Dalouche
+ * Copyright 2011 Sami Dalouche
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.sirika.hchelpers
 
 import org.apache.http.client.HttpClient
